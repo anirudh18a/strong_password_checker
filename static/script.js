eb43189bec9362db3b48249975573a86f4dd9a7e
anirudh18a <anirudhsaha17@gmail.com>
2026-05-31 12:41:50 +0530

    change mideum strength to 4

--- static/script.js
+++ static/script.js
@@ -25,7 +25,7 @@ function checkPassword() {
         result.innerHTML = "Weak Password";
         result.style.color = "red";
     }
-    else if (strength <= 3) {
+    else if (strength <= 4) {
         result.innerHTML = "Medium Password";
         result.style.color = "orange";
     }
